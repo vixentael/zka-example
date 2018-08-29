5e038e975f2695ac9f71988e77879af53737ef16
Vixentael <vixentael@gmail.com>
2018-08-30 02:14:45 +0300

    decode base64

--- SecurePostsExample/Encryption/EncryptionEngine+DecryptOtherPost.swift
+++ SecurePostsExample/Encryption/EncryptionEngine+DecryptOtherPost.swift
@@ -25,24 +25,31 @@ extension EncryptionEngine {
   }
   
   func decryptSomebodyPost(encryptedPost: EncryptedData, author: String) throws -> String {
-    // 1. make sure we can decrypt user's SK
-    guard let kp = hasKeysToDecryptSomebodyPost(user: author) else {
-      throw EncryptionError.cantDecryptOtherPostNoKeys
-    }
+    // TODO: uncomment below to implement decryption
+    return try decryptAnyPost(encryptedPost:encryptedPost, secretKey:Key(string: "not real key, because there's no decryption in this branch")!)
     
-    // 2. decrypt SK
-    let userSecretKey = Key(string:
-        try decryptSecretKeyFromUser(
-            encryptedSecretKey: kp.encryptedSharedKey,
-            userPublicKey: kp.publicKey)
-        )!
-    
-    // 3. decrypt post
-    return try decryptAnyPost(encryptedPost:encryptedPost, secretKey:userSecretKey)
+//    // 1. make sure we can decrypt user's SK
+//    guard let kp = hasKeysToDecryptSomebodyPost(user: author) else {
+//      throw EncryptionError.cantDecryptOtherPostNoKeys
+//    }
+//
+//    // 2. decrypt SK
+//    let userSecretKey = Key(string:
+//        try decryptSecretKeyFromUser(
+//            encryptedSecretKey: kp.encryptedSharedKey,
+//            userPublicKey: kp.publicKey)
+//        )!
+//
+//    // 3. decrypt post
+//    return try decryptAnyPost(encryptedPost:encryptedPost, secretKey:userSecretKey)
   }
   
   func decryptAnyPost(encryptedPost: EncryptedData, secretKey: Key) throws -> String {
     // TODO: implement decryption
+    if let decodedPost = String(data: encryptedPost.data, encoding: .utf8) {
+      return decodedPost
+    }
+    
     return encryptedPost.base64String
 
     // 1. create decryptor with own secret key
